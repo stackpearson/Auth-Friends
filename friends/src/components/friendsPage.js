485a6f587e2daac5a28a249787cefe81c236a2bd
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-08-07 22:27:15 -0600

    succesfully pulling from API upon login

--- friends/src/components/friendsPage.js
+++ friends/src/components/friendsPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
-
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 class friendsPage extends React.Component {
+
+    componentDidMount() {
+        this.pullFriends();
+      }
+
+    pullFriends = () => {
+        axiosWithAuth().get('http://localhost:5000/api/friends')
+        .then((res) => {
+            console.log('succesful friends call', res)
+        })
+        .catch(err => console.err('failed friends call', err.message))
+    }
 
     render() {
         return (
